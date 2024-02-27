9e85ec9381c6b778ebf005be7f9da52ddd43ebe4
Orhan Isik <isiko979@gmail.com>
2024-02-28 01:31:26 +0300

    modified VendorServiceImpl

--- src/main/java/com/trade/crm/service/impl/VendorServiceImpl.java
+++ src/main/java/com/trade/crm/service/impl/VendorServiceImpl.java
@@ -1,20 +1,39 @@
 package com.trade.crm.service.impl;
 
 import com.trade.crm.dto.VendorDTO;
+import com.trade.crm.entity.Vendor;
+import com.trade.crm.repository.VendorRepository;
 import com.trade.crm.service.VendorService;
+import com.trade.crm.util.MapperUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VendorServiceImpl implements VendorService {
+
+    private final VendorRepository vendorRepository;
+    private final MapperUtil mapperUtil;
+
+    public VendorServiceImpl(VendorRepository vendorRepository, MapperUtil mapperUtil) {
+        this.vendorRepository = vendorRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
     @Override
     public void save(VendorDTO vendorDTO) {
+
+        vendorRepository.save(mapperUtil.convert(vendorDTO, new Vendor()));
 
     }
 
     @Override
     public List<VendorDTO> listAllVendors() {
-        return null;
+
+        List<Vendor> vendorList = vendorRepository.findAll();
+
+        return vendorList.stream().map(vendor -> mapperUtil.convert(vendor, new VendorDTO()))
+                .collect(Collectors.toList());
     }
 }
