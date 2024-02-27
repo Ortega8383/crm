07691b9f40010e646ba7389ceccd98a26e1afc6c
Orhan Isik <isiko979@gmail.com>
2024-02-28 01:12:37 +0300

    added VendorRepository

---
+++ src/main/java/com/trade/crm/repository/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.trade.crm.repository;
+
+import com.trade.crm.entity.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendorRepository extends JpaRepository<Vendor, Integer> {
+}
