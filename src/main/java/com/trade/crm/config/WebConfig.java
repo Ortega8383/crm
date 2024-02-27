bbe27fa6439b070f3d57dde69de0633bc132152e
Orhan Isik <isiko979@gmail.com>
2024-02-27 16:17:25 +0300

    added welcome.html

--- src/main/java/com/trade/crm/config/WebConfig.java
+++ src/main/java/com/trade/crm/config/WebConfig.java
@@ -8,5 +8,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/welcome").setViewName("welcome");
     }
 }
