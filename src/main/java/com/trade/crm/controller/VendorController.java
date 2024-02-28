99090d8dfd91595daf26becf88b41314ea9e1de3
Orhan Isik <isiko979@gmail.com>
2024-02-28 14:17:13 +0300

    added validation

--- src/main/java/com/trade/crm/controller/VendorController.java
+++ src/main/java/com/trade/crm/controller/VendorController.java
@@ -5,10 +5,13 @@ import com.trade.crm.entity.Vendor;
 import com.trade.crm.service.VendorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/vendor")
@@ -28,7 +31,12 @@ public class VendorController {
         return "vendor/vendor-create";
     }
     @PostMapping("/insert")
-    public String insertVendor(@ModelAttribute("vendor") VendorDTO vendorDTO, Model model) {
+    public String insertVendor(@ModelAttribute("vendor") @Valid VendorDTO vendorDTO, BindingResult bindingResult, Model model) {
+
+        if(bindingResult.hasErrors()) {
+            return "vendor/vendor-create";
+        }
+
         vendorService.save(vendorDTO);
         model.addAttribute("vendor", new VendorDTO());
         return "vendor/vendor-success";
--- src/main/java/com/trade/crm/dto/VendorDTO.java
+++ src/main/java/com/trade/crm/dto/VendorDTO.java
@@ -5,8 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Where;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 @Getter
 @Setter
@@ -14,18 +16,41 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class VendorDTO {
     private int id;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String company;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String firstName;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String lastName;
+    @NotNull
     private Gender gender;
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+    @NotBlank
+    @Email
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
     private String email;
+    @NotBlank
+    @Pattern(regexp = "^\\+?\\d{1,2}\\s?\\(?\\d{3}\\)?[-.\\s]?\\d{3}[-.\\s]?\\d{4}$")
     private String phone;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String addressLine1;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String addressLine2;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String town;
+    @NotBlank
+    @Size(max = 20, min = 2)
     private String city;
+    @NotBlank
+    @Size(max = 7, min = 2)
     private String zipCode;
     private String notes;
 }
